d03238133b844917398486e66b01193ff9ce0931
Comicome <come.dlvl@gmail.com>
2018-03-14 18:21:47 +0100

    Update of the entity Match

--- src/Entity/Match.php
+++ src/Entity/Match.php
@@ -35,4 +35,99 @@ class Match
      * @ORM\Column(type="string")
      */
     protected $score;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $game_id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $created;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $updated;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTeamA()
+    {
+        return $this->teamA;
+    }
+
+    /**
+     * @param mixed $teamA
+     */
+    public function setTeamA($teamA)
+    {
+        $this->teamA = $teamA;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTeamB()
+    {
+        return $this->teamB;
+    }
+
+    /**
+     * @param mixed $teamB
+     */
+    public function setTeamB($teamB)
+    {
+        $this->teamB = $teamB;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getScore()
+    {
+        return $this->score;
+    }
+
+    /**
+     * @param mixed $score
+     */
+    public function setScore($score)
+    {
+        $this->score = $score;
+    }
 }
