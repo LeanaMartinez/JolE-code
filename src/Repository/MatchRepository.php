c8d616b104b73e12af29664c197e4714c8371132
Comicome <come.dlvl@gmail.com>
2018-03-15 14:42:07 +0100

    Refactor Match -> Matches

--- src/Repository/MatchRepository.php
+++ src/Repository/MatchRepository.php
@@ -7,16 +7,16 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 /**
- * @method Match|null find($id, $lockMode = null, $lockVersion = null)
- * @method Match|null findOneBy(array $criteria, array $orderBy = null)
- * @method Match[]    findAll()
- * @method Match[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method Matches|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Matches|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Matches[]    findAll()
+ * @method Matches[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class MatchRepository extends ServiceEntityRepository
 {
     public function __construct(RegistryInterface $registry)
     {
-        parent::__construct($registry, Match::class);
+        parent::__construct($registry, Matches::class);
     }
 
     /*
