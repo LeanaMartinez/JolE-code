4b0dc5302bcbc6769eb6454338953c7c6d431541
marchassaigne <marchassaigne@gmail.com>
2018-03-22 16:12:27 +0100

    Update setTo & indentation

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -41,7 +41,7 @@ class RegistrationController extends Controller
 // ... do any other work - like sending them an email, etc
             $message = (new \Swift_Message('Confirmation de compte'))
                 ->setFrom(['jolesport.iesa@gmail.com' => 'JolE-sport'])
-                ->setTo('jolesport.iesa@gmail.com')
+                ->setTo($user->getEmail())
                 ->setBody('Votre compte JolE-sport a bien été créé !');
 
             $mailer->send($message);
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -13,23 +13,22 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
-public function buildForm(FormBuilderInterface $builder, array $options)
-{
-$builder
-->add('email', EmailType::class)
-->add('username', TextType::class)
-->add('plainPassword', RepeatedType::class, array(
-'type' => PasswordType::class,
-'first_options'  => array('label' => 'Password'),
-'second_options' => array('label' => 'Repeat Password'),
-))
-;
-}
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('username', TextType::class)
+            ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options' => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password'),
+            ));
+    }
 
-public function configureOptions(OptionsResolver $resolver)
-{
-$resolver->setDefaults(array(
-'data_class' => User::class,
-));
-}
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => User::class,
+        ));
+    }
 }
